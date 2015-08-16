2a30b49fe7316f757f05bd94930d9d8ac215fd70
Dean Brettle <dean@brettle.com>
2015-08-15 21:30:37 -0700

    Use createUser() instead of login() and log to page.
    The switch from login() to createUser() is just to make the test run entirely
    on the client side. Using login() works as well.

--- login-failure-breaks-reconnect.js
+++ login-failure-breaks-reconnect.js
@@ -1,61 +1,68 @@
-var username1 = 'testuser1';
-var username2 = 'testuser2';
-var password1 = 'password1'
-var password2 = 'password2'
-
 if (Meteor.isServer) {
   Meteor.methods({
-    getConnectionUserId: function() {
-       return this.userId;
+    getConnectionUserId: function () {
+      return this.userId;
     }
-  });
-  Meteor.users.remove({username: username1});
-  Meteor.users.remove({username: username2});
-  Accounts.createUser({
-    username: username1,
-    password: password1
-  });
-  Accounts.createUser({
-    username: username2,
-    password: password2
   });
 }
 
 if (Meteor.isClient) {
+  var username1 = 'testuser1-' + Random.id();
+  var username2 = 'testuser2-' + Random.id();
+  var password1 = 'password1-' + Random.id();
+  var password2 = 'password2-' + Random.id();
   var timeoutHandle;
-  Meteor.startup(function () {
-    console.log('Logging in as: ' + username1);
-    Meteor.loginWithPassword(username1, password1, onUser1LoggedIn);
+
+  Template.body.helpers({
+    logContents: function () {
+      return Session.get('logContents');
+     }
+  });
+
+  function log(msg) {
+    console.log(msg);
+    Session.set('logContents', Session.get('logContents') + msg + '\n');
+  }
+
+  Meteor.startup(function() {
+    Session.set('logContents', '');
+    log('Logging in as: ' + username1);
+    Accounts.createUser({
+      username: username1,
+      password: password1
+    }, onUser1LoggedIn);
   });
 
   function onUser1LoggedIn(err) {
     check(err, undefined); // No error
-    console.log('Successfully logged in as: ' + Meteor.user().username);
-    console.log('Logging in as: ' + username2);
-    Meteor.loginWithPassword(username2, password2, onUser2LoggedIn);
+    log('Successfully logged in as: ' + Meteor.user().username);
+    log('Logging in as: ' + username2);
+    Accounts.createUser({
+      username: username2,
+      password: password2
+    }, onUser2LoggedIn);
   }
 
   function onUser2LoggedIn(err) {
     check(err, undefined); // No error
-    console.log('Successfully logged in as: ' + Meteor.user().username);
-    console.log('Forcing a reconnect should cause us to relogin...');
+    log('Successfully logged in as: ' + Meteor.user().username);
+    log('Forcing a reconnect should cause us to relogin...');
     Accounts.onLogin(onUser2LoggedInAfterReconnect);
     Meteor.disconnect();
     Meteor.reconnect();
   }
 
   function onUser2LoggedInAfterReconnect() {
-    console.log('Logged in after reconnect as: ' + Meteor.user().username);
-    console.log('Intentionally causing login failure...');
+    log('Logged in after reconnect as: ' + Meteor.user().username);
+    log('Intentionally causing login failure...');
     Meteor.loginWithPassword('non-existent-user', 'or-wrong-password',
       onFailedLogin);
   }
 
   function onFailedLogin(err) {
     check(err, Meteor.Error); // We expected an error
-    console.log('Login failed (as planned) but we are still correctly logged in as: '
-      + Meteor.user().username);
-    console.log('Forcing reconnect should still cause us to relogin');
+    log('Login failed (as planned) but we are still correctly logged in as: ' + Meteor.user().username);
+    log('Forcing reconnect should still cause us to relogin');
     Accounts.onLogin(onUser2LoggedInAfterReconnectAfterFailedLogin);
     Meteor.disconnect();
     Meteor.reconnect();
@@ -63,26 +70,25 @@ if (Meteor.isClient) {
   }
 
   function sayBugStillPresent() {
-    console.log('Login on reconnect does not work after login failure');
+    log('BUG: Login on reconnect does NOT work after login failure!');
     Meteor.call('getConnectionUserId', showFinalState);
   }
 
   function onUser2LoggedInAfterReconnectAfterFailedLogin() {
     Meteor.clearTimeout(timeoutHandle);
-    console.log('The bug is fixed. We were relogged in as:'
-      + Meteor.user().username);
+    log('The bug is fixed. We were relogged in as:' + Meteor.user().username);
     Meteor.call('getConnectionUserId', showFinalState);
   }
 
   function showFinalState(err, connectionUserId) {
     check(err, undefined); // No error
-    console.log('this.userId in server method: ' + connectionUserId);
-    console.log('Meteor.userId() on client: ' + Meteor.userId());
+    log('this.userId in server method: ' + connectionUserId);
+    log('Meteor.userId() on client: ' + Meteor.userId());
     var user = Meteor.user();
     if (user) {
-      console.log('Meteor.user().username on client: ' + user.username);
+      log('Meteor.user().username on client: ' + user.username);
     } else {
-      console.log('Meteor.user() on client: ' + user);
+      log('Meteor.user() on client: ' + user);
     }
   }
 }
